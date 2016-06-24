bdab953da4251196d8cfa55306cbf8ba46b56e8f
Geir Ove Grønmo <grove@geirove.org>
2016-06-24 19:47:14 +0200

    Fixed since query param

--- src/main/java/io/sesam/examples/App.java
+++ src/main/java/io/sesam/examples/App.java
@@ -9,23 +9,24 @@ import com.google.gson.Gson;
 
 public class App {
 
-	private static Gson gson = new Gson();
-	
+    private static Gson gson = new Gson();
+
     public static void main(String[] args) {
-    	Repository<Entity> repository = new EntityRepository();
+        Repository<Entity> repository = new EntityRepository();
         get("/entities", (req, res) -> {
-        	String since = req.params("since");
-        	res.type("application/json");
-        	StringWriter sw = new StringWriter();
-        	sw.append("[");
-        	Iterator<? extends Entity> iter = repository.getEntities(since);
+            res.type("application/json");
+            String since = req.queryParams("since");
+            Iterator<? extends Entity> iter = repository.getEntities(since);
+
+            StringWriter sw = new StringWriter();
+            sw.append("[");
             while (iter.hasNext()) {
-            	sw.append(gson.toJson(iter.next()));
-            	if (iter.hasNext()) {
-            		sw.append(",");
-            	}
+                sw.append(gson.toJson(iter.next()));
+                if (iter.hasNext()) {
+                    sw.append(",");
+                }
             };
-        	sw.append("]");
+            sw.append("]");
             return sw.toString();
         });
     }
